491f0538a8d61f2fb52edbe79de65892f03c5820
Faik TURAN <faikturan@users.noreply.github.com>
2022-05-06 09:51:02 +0300

    Initialize replace-i18n-package

---
+++ src/helpers/utils.tsx
@@ -0,0 +1,39 @@
+const utils = {
+    /**
+     * Generates a random string.
+     * @param length The result length
+     * @returns A random string
+     */
+    randomString(length: number) {
+        let result = "";
+        const possible = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+        for (let i = 0; i < length; i++) {
+            result += possible.charAt(Math.floor(Math.random() * possible.length));
+        }
+        return result;
+    },
+    /**
+     * Deplays to execute a promise like ajax call.
+     * @param seconds The delay seconds
+     * @param promise The promise to delay
+     * @returns A promise
+     */
+    deplay<T>(seconds: number, promise: Promise<T>): Promise<T> {
+        return new Promise((resolve, reject) => {
+            setTimeout(() => {
+                if (promise) {
+                    promise.then(
+                        (res: T) => {
+                            resolve(res);
+                        },
+                        (err: any) => {
+                            reject(err);
+                        },
+                    );
+                }
+            }, seconds * 1000);
+        });
+    },
+};
+
+export default utils;
---
+++ src/service/resource.tsx
@@ -0,0 +1,23 @@
+import { notify } from "@bndynet/dialog";
+import { config } from "../config";
+import { Ajax } from "../helpers/ajax";
+import { getState } from "./auth";
+
+class ResourceService extends Ajax {
+    constructor() {
+        super({
+            headerAuthorization: () => {
+                if (getState().token) {
+                    return `${getState().token.token_type} ${getState().token.access_token}`;
+                }
+                return "";
+            },
+            baseURL: config.resourceBaseUri,
+            onResponseError: error => {
+                notify(JSON.stringify(error), "error");
+            },
+        });
+    }
+}
+
+export const service = new ResourceService();
